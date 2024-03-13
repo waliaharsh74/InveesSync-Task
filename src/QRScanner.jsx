1544d14b70e2dc5411babc9134af22791d3f942a
“Harsh <harshwalia449@gmail.com>
2024-03-13 09:18:01 +0530

    hoping rear camera added take3

--- src/QRScanner.jsx
+++ src/QRScanner.jsx
@@ -4,21 +4,34 @@ import { QrReader } from "react-qr-reader";
 function QRScanner({ updateLoc }) {
   const [data, setData] = useState('No result');
   const [constraints, setConstrains] = useState({
-  facingMode: { exact: "environment" },
+  // facingMode: { exact: "environment" },
+  facingMode: "environment",
 });
 
 
 
 
+  // const toggleCameraFacingMode = () => {
+  //   const value=constraints.facingMode.exact
+  //   console.log(constraints.facingMode.exact);
+  //   if(value==="user"){
+  //     setConstrains({facingMode: { exact: "environment" }})
+      
+  //   }
+  //   else{
+  //     setConstrains({facingMode: { exact: "user" }})
+
+  //   }
+  // };
   const toggleCameraFacingMode = () => {
-    const value=constraints.facingMode.exact
-    console.log(constraints.facingMode.exact);
+    const value=constraints.facingMode
+    console.log(constraints.facingMode);
     if(value==="user"){
-      setConstrains({facingMode: { exact: "environment" }})
+      setConstrains({facingMode: "environment"})
       
     }
     else{
-      setConstrains({facingMode: { exact: "user" }})
+      setConstrains({facingMode: "user"})
 
     }
   };
